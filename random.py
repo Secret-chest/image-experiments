81e8e467053109e008efd621171e992ac805801c
vlad <vlad.muntoiu@gmail.com>
2021-12-22 22:03:54 +0200

    Ok

--- random.py
+++ random.py
@@ -1,11 +1,8 @@
-import cv2
 import numpy
 from PIL import Image, ImageShow
 
 imgWidth = 1280
 imgHeight = 720
-
-print(imgWidth * imgHeight * 256 ** 3)
 
 colorArray = numpy.random.rand(imgHeight, imgWidth, 3) * 255
 colorImg = Image.fromarray(colorArray.astype("uint8")).convert("RGB")
@@ -15,9 +12,5 @@ grayscaleArray = numpy.random.rand(imgHeight, imgWidth, 3) * 255
 grayscaleImg = Image.fromarray(grayscaleArray.astype("uint8")).convert("L")
 grayscaleImg.save("randomGrayscale.bmp")
 
-grayscaleImg = cv2.imread("randomGrayscale.bmp")
-colorImg = cv2.imread("randomColors.bmp")
-
-cv2.imshow("Grayscale", grayscaleImg)
-cv2.imshow("Color", colorImg)
-cv2.waitKey(0)
+ImageShow.show(grayscaleImg, "Grayscale")
+ImageShow.show(colorImg, "Color")

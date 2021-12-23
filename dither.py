3838b49a1fe79bfd3ddeaefb222a3605721b61bf
vlad <vlad.muntoiu@gmail.com>
2021-12-23 21:30:22 +0200

    Oops

--- dither.py
+++ dither.py
@@ -2,8 +2,6 @@ from PIL import Image, ImageShow, ImagePalette
 image = "sampleImages/EiffelTower.jpg"
 viewImages = True
 
-CGA = ImagePalette.ImagePalette(mode="RGB", palette=((255, 255, 255, 0, 0, 0)))
-
 # Open image
 with Image.open(image) as inputImage:
     # Reduce to black and white

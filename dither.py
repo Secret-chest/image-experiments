a6321eb08d44d5787e360a68e8536df0bab04adb
vlad <vlad.muntoiu@gmail.com>
2021-12-24 16:23:35 +0200

    Dither now saves a .bmp

--- dither.py
+++ dither.py
@@ -8,7 +8,7 @@ with Image.open(image) as inputImage:
     # Reduce to black and white
     ditheredImage = inputImage.convert("1")
     # Save
-    ditheredImage.save("dither/dithered.png")
+    ditheredImage.save("dither/dithered.bmp")
 
     if viewImages:
         ImageShow.show(ditheredImage, "Dithered")

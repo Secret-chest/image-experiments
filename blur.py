9382176156c3c40c43934dffd5105fe3b6bf451f
vlad <vlad.muntoiu@gmail.com>
2021-12-24 11:35:25 +0200

    New script - blur

---
+++ blur.py
@@ -0,0 +1,45 @@
+from PIL import Image, ImageShow, ImageFilter
+
+image = "sampleImages/Hills.jpg"
+viewImages = False
+
+with Image.open(image) as inputImage:
+    # Filter
+    blurredImage = inputImage.filter(ImageFilter.BLUR)
+    # Save
+    blurredImage.save("blur/normalBlur.png")
+
+    if viewImages:
+        ImageShow.show(blurredImage, "Normal blur")
+
+    # Filter
+    blurredImage2 = inputImage.filter(ImageFilter.GaussianBlur(2))
+    # Save
+    blurredImage2.save("blur/gaussianBlur.png")
+
+    if viewImages:
+        ImageShow.show(blurredImage2, "Gaussian blur")
+
+    # Filter
+    blurredImage3 = inputImage.filter(ImageFilter.GaussianBlur(4))
+    # Save
+    blurredImage3.save("blur/gaussianBlurExtra.png")
+
+    if viewImages:
+        ImageShow.show(blurredImage3, "More Gaussian blur")
+
+    # Filter
+    blurredImage4 = inputImage.filter(ImageFilter.BoxBlur(4))
+    # Save
+    blurredImage4.save("blur/boxBlur.png")
+
+    if viewImages:
+        ImageShow.show(blurredImage4, "Box blur")
+
+    # Filter
+    blurredImage5 = inputImage.filter(ImageFilter.BoxBlur(8))
+    # Save
+    blurredImage5.save("blur/boxBlurExtra.png")
+
+    if viewImages:
+        ImageShow.show(blurredImage5, "More box blur")

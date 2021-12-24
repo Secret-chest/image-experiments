3e380c0685e85a16f25d7a59969a648cdeb63ff6
vlad <vlad.muntoiu@gmail.com>
2021-12-24 21:20:58 +0200

    Smooth/sharpen scripts

---
+++ sharpen.py
@@ -0,0 +1,13 @@
+from PIL import Image, ImageShow, ImageFilter
+
+image = "sampleImages/EiffelTower.jpg"
+viewImages = False
+
+with Image.open(image) as inputImage:
+    # Filter
+    sharpenedImage = inputImage.filter(ImageFilter.SHARPEN)
+    # Save
+    sharpenedImage.save("sharpen/sharpened.png")
+
+    if viewImages:
+        ImageShow.show(sharpenedImage, "Sharpened")
---
+++ smooth.py
@@ -0,0 +1,21 @@
+from PIL import Image, ImageShow, ImageFilter
+
+image = "sampleImages/EiffelTower.jpg"
+viewImages = False
+
+with Image.open(image) as inputImage:
+    # Filter
+    smoothedImage = inputImage.filter(ImageFilter.SMOOTH)
+    # Save
+    smoothedImage.save("smooth/smoothed.png")
+
+    if viewImages:
+        ImageShow.show(smoothedImage, "Smoothed")
+
+    # Filter
+    smoothedImage2 = inputImage.filter(ImageFilter.SMOOTH_MORE)
+    # Save
+    smoothedImage2.save("smooth/smoothedExtra.png")
+
+    if viewImages:
+        ImageShow.show(smoothedImage2, "More smoothed")

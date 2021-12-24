7bab4f93321445088e0c9dc870c46d2331bb5053
vlad <vlad.muntoiu@gmail.com>
2021-12-24 11:01:12 +0200

    New sample image: saturn.jpg

--- sketch.py
+++ sketch.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageShow, ImageFilter
 
-image = "sampleImages/BotanicGarden.jpg"
+image = "sampleImages/saturn.jpg"
 viewImages = False
 
 with Image.open(image) as inputImage:
